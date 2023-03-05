978ef6ffb729de934d4782dfbf84e3794871aece
Mairene-Villasmil <mairenevillasmilf@gmail.com>
2023-03-05 18:36:04 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+comsole.log(JS)
+
+var edad = 11
+
+if(edad >= 18){
+    console.log("Esta en edad para trabajar")
+} else {
+
+    if(edad >= 12 && edad < 18)
+    {
+        console.log("Debe estar en secundaria")
+    }else
+    {
+        if (edad >= 5 && edad < 12){
+        console.log("Debe estar en primaria")
+    }else {
+        console.log("es muy chico para estudiar")
+    }
+}
+}
